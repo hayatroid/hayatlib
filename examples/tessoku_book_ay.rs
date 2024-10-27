d71fc32bb57cdabebd2ab8c356b3d48f64c0aedb
hayatroid <hayatroid@gmail.com>
2024-10-27 13:52:18 +0900

    Refactor slightly

--- examples/tessoku_book_ay.rs
+++ examples/tessoku_book_ay.rs
@@ -9,12 +9,8 @@ fn main() {
     let mut stack = vec![];
     for query in queries {
         match query {
-            Query::Q1(x) => {
-                stack.push(x);
-            }
-            Query::Q2 => {
-                println!("{}", stack.last().unwrap());
-            }
+            Query::Q1(x) => stack.push(x),
+            Query::Q2 => println!("{}", stack.last().unwrap()),
             Query::Q3 => {
                 stack.pop();
             }
