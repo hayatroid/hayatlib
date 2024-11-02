52b84e40b5af6e331feb93fec782c4d0788eeeb8
hayatroid <hayatroid@gmail.com>
2024-11-02 13:51:28 +0900

    Refactoring

--- src/math/factorize_fast.rs
+++ src/math/factorize_fast.rs
@@ -8,12 +8,16 @@ fn gcd(x: u64, y: u64) -> u64 {
 }
 
 fn factorize_inner(n: u64) -> Vec<u64> {
-    let non_trivial_divisor_of = |n: u64| -> Option<u64> {
-        if is_prime(n) {
-            return None;
-        }
+    assert!(n != 0);
+    if n == 1 {
+        return vec![];
+    }
+    if is_prime(n) {
+        return vec![n];
+    }
+    let non_trivial_divisor_of = |n: u64| -> u64 {
         if n % 2 == 0 {
-            return Some(2);
+            return 2;
         }
         for step in 1.. {
             let f = |x: u64| -> u64 { ((x as u128 * x as u128 + step as u128) % n as u128) as u64 };
@@ -26,26 +30,19 @@ fn factorize_inner(n: u64) -> Vec<u64> {
                 d = gcd(x.abs_diff(y), n);
             }
             if d != n {
-                return Some(d);
+                return d;
             }
         }
         unreachable!()
     };
-    if let Some(d) = non_trivial_divisor_of(n) {
-        let mut l = factorize(d);
-        let r = factorize(n / d);
-        l.extend(r);
-        l
-    } else {
-        vec![n]
-    }
+    let d = non_trivial_divisor_of(n);
+    let mut l = factorize_inner(d);
+    let r = factorize_inner(n / d);
+    l.extend(r);
+    l
 }
 
 pub fn factorize(n: u64) -> Vec<u64> {
-    assert!(n != 0);
-    if n == 1 {
-        return vec![];
-    }
     let mut res = factorize_inner(n);
     res.sort();
     res
