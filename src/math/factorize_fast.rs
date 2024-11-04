607ee38f498220545c745137bef2810977cc95ed
hayatroid <hayatroid@gmail.com>
2024-11-04 18:54:39 +0900

    Refactoring slightly

--- src/math/factorize_fast.rs
+++ src/math/factorize_fast.rs
@@ -15,27 +15,26 @@ fn factorize_inner(n: u64) -> Vec<u64> {
     if is_prime(n) {
         return vec![n];
     }
-    let divisor = (1..)
-        .find_map(|step| {
-            if n % 2 == 0 {
-                return Some(2);
-            }
-            let f = |x| ((x as u128 * x as u128 + step as u128) % n as u128) as u64;
-            let mut x = step;
-            let mut y = f(x);
-            let mut d = 1;
-            while d == 1 {
-                x = f(x);
-                y = f(f(y));
-                d = gcd(x.abs_diff(y), n);
-            }
-            Some(d).filter(|&d| d != n)
-        })
-        .unwrap();
-    let mut l = factorize_inner(divisor);
-    let r = factorize_inner(n / divisor);
-    l.extend(r);
-    l
+    let find_divisor_of_n = |seed: u64| -> Option<u64> {
+        let f = |x| ((x as u128 * x as u128 + seed as u128) % n as u128) as u64;
+        let mut x = seed;
+        let mut y = f(x);
+        let mut d = 1;
+        while d == 1 {
+            x = f(x);
+            y = f(f(y));
+            d = gcd(x.abs_diff(y), n);
+        }
+        Some(d).filter(|&d| d != n)
+    };
+    let divisor = if n % 2 == 0 {
+        2
+    } else {
+        (1..).find_map(find_divisor_of_n).unwrap()
+    };
+    let mut res = factorize_inner(divisor);
+    res.extend(factorize_inner(n / divisor));
+    res
 }
 
 pub fn factorize(n: u64) -> Vec<u64> {
